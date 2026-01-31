a38aba87aba033a754412dde6a5bc1fb3c5959fe
AmrSaed55 <166567685+AmrSaed55@users.noreply.github.com>
2026-02-01 01:46:30 +0200

    Responsive Design Update

---
+++ js/main.js
@@ -0,0 +1,26 @@
+let theBar = document.getElementById("bar");
+let theNav = document.querySelector("nav ul");
+let theCloseIcon = document.getElementById("close");
+let upBtn = document.getElementById("to-up-btn");
+
+if (theBar) {
+  theBar.addEventListener("click", () => {
+    theNav.classList.add("active");
+  });
+}
+
+if (theCloseIcon) {
+  theCloseIcon.addEventListener("click", () => {
+    theNav.classList.remove("active");
+  });
+}
+
+document.onscroll = () => {
+  if (upBtn) {
+    if (scrollY >= 1827) {
+      upBtn.style.display = "initial";
+    } else {
+      upBtn.style.display = "none";
+    }
+  }
+};
