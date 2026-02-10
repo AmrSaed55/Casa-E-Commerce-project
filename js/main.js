bf08a1cbbed899dadcf1ab0a48384d2f009025b5
AmrSaed55 <166567685+AmrSaed55@users.noreply.github.com>
2026-02-10 13:08:45 +0200

    About Page is added and responsive

--- js/main.js
+++ js/main.js
@@ -1,26 +1,32 @@
-let theBar = document.getElementById("bar");
-let theNav = document.querySelector("nav ul");
-let theCloseIcon = document.getElementById("close");
-let upBtn = document.getElementById("to-up-btn");
+let theBar = document.getElementById('bar');
+let theNav = document.querySelector('nav ul');
+let theCloseIcon = document.getElementById('close');
+let upBtn = document.getElementById('to-up-btn');
 
 if (theBar) {
-  theBar.addEventListener("click", () => {
-    theNav.classList.add("active");
+  theBar.addEventListener('click', () => {
+    theNav.classList.add('active');
   });
 }
 
+document.addEventListener('click', (e) => {
+  if (e.target !== theBar && e.target !== theNav) {
+    theNav.classList.remove('active');
+  }
+});
+
 if (theCloseIcon) {
-  theCloseIcon.addEventListener("click", () => {
-    theNav.classList.remove("active");
+  theCloseIcon.addEventListener('click', () => {
+    theNav.classList.remove('active');
   });
 }
 
 document.onscroll = () => {
   if (upBtn) {
     if (scrollY >= 1827) {
-      upBtn.style.display = "initial";
+      upBtn.style.display = 'initial';
     } else {
-      upBtn.style.display = "none";
+      upBtn.style.display = 'none';
     }
   }
 };
